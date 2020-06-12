6108721c60c2bdea3777b94b8439965971c2bb8a
Roz Barnes <rozbarnes@gmail.com>
2020-06-12 03:37:25 +0000

    I worked on colors, spacing, borders, buttons. I tried to add event listener but will work on tomorrow I'm not sure if my main.css and main.js files are linked, but it looks like they are to me. I'm trying to loosely recreate the calculator on the iPhone

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 
+
+
+
+//Display
+// function dis(value) {
+//     document.getElementById("edu").value += value;
+// }
+
+//Calculate
+// function solve(id) {
+//     let x = document.getElementById("edu").value
+//     let y = eval(x)
+//     document.getElementById("edu").value = y
+// }
+
+//Clear
+// function clr() {
+//     let x = document.getElementById("edu").value
+//     let y = eval(x)
+//     let z = y - y
+//     document.getElementById("edu").value = z
+// }//
